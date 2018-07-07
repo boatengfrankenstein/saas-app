d9e8a996e854e9ee25e1bea77aae95b64442ece8
daniel boakye <boatengfrankenstein@gmail.com>
2018-07-07 08:02:35 -0400

    Config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.default_url_options = { :host => 'https://saas-app-development.herokuapp.com', :protocol => 'https'}
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
